deb5fd3b11a37128ed549747be6733dd917fffa9
podhmo <ababjam61@gmail.com>
2010-09-29 02:20:54 +0900

    -

--- example/example6.scm
+++ example/example6.scm
@@ -1,6 +1,5 @@
 ;;
 ;; texture with gl-tex-coord
-;;
 
 (add-load-path "..")
 (use gl.processing)
@@ -19,35 +18,25 @@
             (image-height img)
             (image-data-gl img :type :texture))))
 
-(define (draw-shape w h)
-  (gl-begin* GL_QUADS
-             (gl-tex-coord '#f32(1.0 0.0)) (gl-vertex (f32vector w 0.0))
-             (gl-tex-coord '#f32(1.0 1.0)) (gl-vertex (f32vector w h))
-             (gl-tex-coord '#f32(0.0 1.0)) (gl-vertex (f32vector 0.0 h))
-             (gl-tex-coord '#f32(0.0 0.0)) (gl-vertex '#f32(0.0 0.0))))
-
 (define disp
   (draw-once$
    (lambda ()
      (with-texture
-      (gl-bind-texture GL_TEXTURE_2D *tex*)
-      (draw-shape *w* *h*)
+      (draw-shape *tex* *w* *h*)
       (translate 300 400)
-      (draw-shape 100 100)
+      (draw-shape *tex* 100 100)
       (translate -50 -50)
-      (draw-shape 100 100)
+      (draw-shape *tex* 100 100)
       (translate -50 -50)
-      (draw-shape 100 100)))))
-
-
+      (draw-shape *tex* 100 100)))))
 
 (define main
   (receive (w h img) (load-image-file *image-file*)
     (set! *w* w) (set! *h* h) 
     (setup$ (lambda ()
               (window 500 500 "texture" 100 100)
-              (gl-tex-env GL_TEXTURE_ENV GL_TEXTURE_ENV_MODE GL_REPLACE)
-              (set! *tex* (image->texture img w h)))
+              (set! *tex* (image->texture img w h))
+              (image->texture img w h))
             :draw disp)))
 
 (main '())
--- gl/processing.scm
+++ gl/processing.scm
@@ -37,11 +37,12 @@
 ;;; experimental
 (define (image->texture img w h)
   (let1 %texture-id (u32vector-ref (gl-gen-textures 1) 0)
-    (gl-bind-texture GL_TEXTURE_2D %texture-id)
     (gl-tex-parameter GL_TEXTURE_2D GL_TEXTURE_WRAP_S GL_REPEAT)
     (gl-tex-parameter GL_TEXTURE_2D GL_TEXTURE_WRAP_T GL_REPEAT)
     (gl-tex-parameter GL_TEXTURE_2D GL_TEXTURE_MAG_FILTER GL_NEAREST)
     (gl-tex-parameter GL_TEXTURE_2D GL_TEXTURE_MIN_FILTER GL_NEAREST)
+
+    (gl-bind-texture GL_TEXTURE_2D %texture-id)
     (gl-tex-image-2d GL_TEXTURE_2D 0 GL_RGB w h 0 GL_RGB GL_UNSIGNED_BYTE img)
     %texture-id))
 
@@ -52,3 +53,16 @@
        (gl-enable GL_TEXTURE_2D)
        action ...
        (gl-disable GL_TEXTURE_2D)))))
+
+(define draw-shape 
+  (let1 %texture-id +inf.0
+    (lambda (id w h)
+      (unless (= %texture-id id)
+        (set! %texture-id id)
+        (gl-bind-texture GL_TEXTURE_2D id))
+      (gl-begin* GL_QUADS
+                 (gl-tex-coord '#f32(0.0 0.0)) (gl-vertex '#f32(0.0 0.0))
+                 (gl-tex-coord '#f32(0.0 1.0)) (gl-vertex (f32vector 0.0 h))
+                 (gl-tex-coord '#f32(1.0 1.0)) (gl-vertex (f32vector w h))
+                 (gl-tex-coord '#f32(1.0 0.0)) (gl-vertex (f32vector w 0.0))
+                 ))))
